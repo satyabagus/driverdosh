72de803acc7b9cb1aed5c49c3d07b8dfb83b008e
satyabagus <satyabagus@users.noreply.github.com>
2016-09-02 10:22:57 +0700

    Add files via upload

---
+++ driverdosh-disqus.js
@@ -0,0 +1,23 @@
+<script type='text/javascript'>
+  var disqus_shortname = &quot;driverdosh&quot;;
+  var disqus_blogger_current_url = &quot;<data:blog.canonicalUrl/>&quot;;
+  if (!disqus_blogger_current_url.length) {
+      disqus_blogger_current_url = &quot;<data:blog.url/>&quot;;
+  }
+  var disqus_blogger_homepage_url = &quot;<data:blog.homepageUrl/>&quot;;
+  var disqus_blogger_canonical_homepage_url = &quot;<data:blog.canonicalHomepageUrl/>&quot;;
+             (function() {
+          var bloggerjs = document.createElement(&quot;script&quot;);
+          bloggerjs.type = &quot;text/javascript&quot;;
+          bloggerjs.async = true;
+          bloggerjs.src = &quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_item.js&quot;;
+          (document.getElementsByTagName(&quot;head&quot;)[0] || document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(bloggerjs);
+      })();
+  (function() {
+      var bloggerjs = document.createElement(&quot;script&quot;);
+      bloggerjs.type = &quot;text/javascript&quot;;
+      bloggerjs.async = true;
+      bloggerjs.src = &quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_index.js&quot;;
+      (document.getElementsByTagName(&quot;head&quot;)[0] || document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(bloggerjs);
+  })();
+</script>
